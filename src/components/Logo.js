dd9fb307909f6c545aa6d128cfcac9a693e01a3c
Epik-Robotik <portal.epik@gmail.com>
2025-03-24 09:40:47 +0300

    logo modified

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,13 +1,14 @@
 // Logo.js
 import React from 'react'
-import logoImage from '../Epik.jpeg' 
+// import logoImage from '../Epik.jpeg'
+import logoImage from '../profSentetik.svg'
 
 function Logo() {
   const url = 'https://profsentetik.com/app/Images/logodark_1679312205.svg'
   return (
     <div style={{ textAlign: 'center', margin: '20px 0' }}>
       <img
-        src={url}
+        src={logoImage || url }
         alt='Logo'
         style={{ maxWidth: '30%', height: '150px' }}
       />
